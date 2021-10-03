a08d83c9357855defc3bb8bafaeb3e4f12d10f5d
gavinyan123 <89555841+gavinyan123@users.noreply.github.com>
2021-10-02 20:43:24 -0400

    myEach and myPush
    Functions were given

---
+++ assignment2.js
@@ -0,0 +1,67 @@
+// FOR EACH //
+Array.prototype.myEach = function() {
+    for(let i = 0; i<this.length; i++){
+        if(this[i] === undefined) continue;
+        callbackFn(this[i], i, this);
+    }
+};
+
+// MAP //
+Array.prototype.myMap = function() {
+
+};
+
+// FILTER //
+Array.prototype.myFilter = function() {
+
+};
+
+// SOME //
+Array.prototype.mySome = function() {
+
+};
+
+// EVERY //
+Array.prototype.myEvery = function() {
+
+};
+
+// REDUCE //
+Array.prototype.myReduce = function() {
+
+};
+
+// INCLUDES //
+Array.prototype.myIncludes = function() {
+
+};
+
+// INDEXOF //
+Array.prototype.myIndexOf = function() {
+
+};
+
+// PUSH //
+Array.prototype.myPush = function() {
+    let arg_i = 0; 
+    let length = this.length;
+    for(let i = length; i<length + args.length; i++){
+        this[i] = args[arg_i];
+        arg_i++;
+    }
+};
+
+// LASTINDEXOF //
+Array.prototype.myLastIndexOf = function() {
+
+};
+
+// KEYS //
+Object.grabKeys = function() {
+
+};
+
+// VALUES //
+Object.grabValues = function() {
+
+};
